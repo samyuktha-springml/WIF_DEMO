6dee054a958f9c3aa7d1126ed5af2b0f84e6de29
samyuktha-springml <nagasamyuktha.reddybathula@springml.com>
2023-11-03 09:00:55 +0530

    reading data from a json file

--- data/pubsub.py
+++ data/pubsub.py
@@ -1,27 +1,28 @@
 import os
+import json
 from google.cloud import pubsub_v1
 
 # Set the environment variable to point to the service account key file
 # os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "key.json"
 
 # Replace these with your Google Cloud Project and Pub/Sub topic details
-# project_id = "arched-campus-400411"
-# topic_id = "topic1"
 project_id = os.getenv("PROJECT_ID")
-topic_id=os.getenv("TOPIC_ID")
+topic_id = os.getenv("TOPIC_ID")
+
+# Read the JSON message data from a file
+with open("message.json", "r") as file:
+    message_data = json.load(file)
+
 # Create a Pub/Sub client
 publisher = pubsub_v1.PublisherClient()
 
 # Create the topic path
 topic_path = publisher.topic_path(project_id, topic_id)
 
-# Define the message to publish
-message_data = "Hello, Pub/Sub!"
-message_bytes = message_data.encode("utf-8")
+# Convert the message data to a JSON string
+message_str = json.dumps(message_data)
+message_bytes = message_str.encode("utf-8")
 
 # Publish the message to the topic
 future = publisher.publish(topic_path, data=message_bytes)
 print(f"Published message {future.result()}")
-
-# # Clean up the client
-# publisher.close()
